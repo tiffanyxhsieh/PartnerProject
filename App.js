c975ad1905fa908d923514669f8f15edffe7bc01
Tiffany Hsieh <tiffanyxhsieh@gmail.com>
2020-01-21 12:56:42 -0500

    integrated graphql and amplify

--- App.js
+++ App.js
@@ -1,14 +1,63 @@
 import React from 'react';
 import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
+import {API, graphqlOperation} from 'aws-amplify';
+
+const ListBooks =`
+query{
+  listBooks{
+    items{
+      id title author
+    }
+  }
+}
+`;
+
+const AddBook = `
+mutation ($title: String! $author: String) {
+  createBook(input: {
+    title: $title
+    author: $author
+  }) {
+    id title author
+  }
+}
+`;
 export default class App extends React.Component {
   state = {
     title: '',
     author: '',
     books: []
   };
+
+  async componentDidMount() {
+    try{
+      const books = await API.graphql(graphqlOperation(ListBooks));
+      console.log('books: ', books);
+      this.setState({books: books.data.listBooks.items});
+    } catch(err) {
+      console.log('error: ', err);
+    }
+  }
   onChangeText = (key, val) => {
     this.setState({ [key]: val });
   };
+  addBook = async () => {
+    if (this.state.title === '' || this.state.author === '') return;
+    const book = { title: this.state.title, author: this.state.author };
+    try {
+      const books = [...this.state.books, book];
+      this.setState({ books, title: '', author: '' });
+      console.log('books: ', books);
+      await API.graphql(graphqlOperation(AddBook, book));
+      console.log('success');
+    } catch (err) {
+      console.log('error: ', err);
+    }
+  };
+
   render() {
     return (
       <View style={styles.container}>
@@ -25,10 +74,19 @@ export default class App extends React.Component {
           placeholder="Who wrote it?"
         />
         <Button
-          onPress={() => alert('Success!')}
+          onPress={this.addBook}
+          style={styles.addButton}
           title="Add to TBR"
           color="#eeaa55"
-        />
+          />
+            {this.state.books.map((book, index) => (
+              <View key={index} style={styles.book}>
+                <Text style={styles.title}>{book.title}</Text>
+                <Text style={styles.author}>{book.author}</Text>
+              </View>
+            ))}
+
+  
       </View>
     );
   }
@@ -43,7 +101,17 @@ const styles = StyleSheet.create({
   input: {
     height: 50,
     borderBottomWidth: 2,
-    borderBottomColor: 'blue',
-    marginVertical: 10
+    marginVertical: 10//,
+    //borderBottomColor
+
+  },
+  title: { 
+    //
+  },
+  author: { 
+    //
+     
   }
+
+
 });
